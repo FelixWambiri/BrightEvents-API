502625d45e2688467a2c5a28f336a8ed1ab18a61
Felix Wambiri <felixwambiri21@gmail.com>
2018-01-03 16:14:33 +0300

    [ADD] Add database configuration settings

--- app/instance/config.py
+++ app/instance/config.py
@@ -7,6 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = 'S\x883\xc6\x01\x07\xc5 \r\xd8\xab\\\xc4{$\xdf\xc6-\x8a\xd1\x85\xd1j'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:AndelaFellow2017@localhost/BrightEventDb'
 
 
 # Test config class that inherits from the default config class
@@ -15,11 +16,13 @@ class TestingConfig(BaseConfig):
     TESTING = True
     WTF_CSRF_ENABLED = False
     PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SQLALCHEMY_ECHO = True  # Helps to debug by logging errors
 
 
 # Development config class that inherits from the default config class
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
+    SQLALCHEMY_ECHO = True  # Helps to debug by logging errors
 
 
 # Production config class that inherits from the default config class
