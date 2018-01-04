50e72a7c3c761828ebd9c511d119c28023691d4a
Felix Wambiri <felixwambiri21@gmail.com>
2018-01-04 14:32:48 +0300

    [ADD] Update user class model to use a database

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,11 @@
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from app.models.event import Event
+from app.views import db
 
-class User(UserMixin):
+
+class User(UserMixin, db.Model):
     """
     Blue print for creating the user of the app
     With all their attributes and methods
@@ -11,64 +14,71 @@ class User(UserMixin):
     def __init__(self, username, email, password):
         self.username = username
         self.id = email
+        self.password = password
+
+    """
+    Create a User table
+    """
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(70), index=True, nullable=False)
+    email = db.Column(db.String(70), index=True, unique=True, nullable=False)
+    password = db.Column(db.String(128))
+    events = db.relationship('Event', backref='user')
+
+    @property
+    def password(self):
+        """
+        Prevent the password from being accessed
+        """
+
+        raise AttributeError('password is not a readable attribute.')
+
+    # Method to hash the password
+    @password.setter
+    def password(self, password):
+        """
+        Set password to a hashed password
+        """
         self.pw_hash = generate_password_hash(password)
-        self.events_dict = {}
 
     # Method to verify the hashed password
     def compare_hashed_password(self, password):
+        """
+        To verify if the hashed password matches the actual password
+        """
         return check_password_hash(self.pw_hash, password)
 
     # Create an event whereby name is key
     # But first check if the event already exists
     def create_event(self, event):
-        if event.name in self.events_dict:
-            raise KeyError("You already have an event by the name" + event.name + "Please use another name")
-        else:
-            return self.events_dict.update({event.name: event})
-
-    # Update an event but first check if the user wants to update that field
-    # If event field is empty previous data is retained
-    # Avoid spaces by using strip function
-    def update_event(self, name, new_name, category, location, owner, description):
-        event = self.events_dict[name]
-        if new_name.strip():
-            event.name = new_name
-            self.events_dict[new_name] = event
-            if new_name != name:
-                del self.events_dict[name]
-        if category.strip():
-            event.category = category
-
-        if location.strip():
-            event.location = location
-
-        if owner.strip():
-            event.owner = owner
-
-        if description.strip():
-            event.description = description
-
-        return event
+        event_f = Event.query.filter_by(event.name).first()
+        if event_f:
+            # Event already exists
+            return False
+        db.session.add(event)
+        db.commit()
+        return True
 
     # Deletes an event but first checks if it exists
-    def delete_event(self, name):
-        if name not in self.events_dict:
-            raise KeyError("There does not exist an event by that name")
-        else:
-            del self.events_dict[name]
+    def delete_event(self, event):
+        db.session.delete(event)
+        db.session.commit()
 
     # This method returns a specific event
-    def get_specific_event(self, event_name):
-        if event_name in self.events_dict:
-            return self.events_dict[event_name]
+    def get_specific_event(self, event):
+        event_f = Event.query.filter_by(event.name).first()
+        if event_f:
+            return event_f
         else:
-            raise KeyError("The event does not exist")
-
-    # Method to return the total number of events
-    def get_number_of_events(self):
-        return len(self.events_dict)
+            print('There is no such event')
+            return False
 
     # Method for User to change password
     def user_reset_password(self, new_pass):
         pass_hash = generate_password_hash(new_pass)
         self.pw_hash = pass_hash
+
+    # Return a printable representation of User class object
+    def __repr__(self):
+        return '<User %r>' % self.email
