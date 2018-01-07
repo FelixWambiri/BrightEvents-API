96f8faa080a001a4c5730ac94cae65af37a2c83f
Felix Wambiri <felixwambiri21@gmail.com>
2018-01-07 22:13:14 +0300

    [ADD] Properly format our database models

--- app/models/event.py
+++ app/models/event.py
@@ -1,4 +1,6 @@
-from app.views import db
+from sqlalchemy import func
+
+from app.init_db import db
 
 
 class Event(db.Model):
@@ -6,17 +8,9 @@ class Event(db.Model):
     This class is the blueprint for creating an event
     It avails the attributes required for an event
     """
-
-    def __init__(self, name, category, location, owner, description):
-        self.name = name
-        self.category = category
-        self.location = location
-        self.owner = owner
-        self.description = description
-
     """
-       Create an Events table
-       """
+        Create an Events table
+    """
     __tablename__ = 'events'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), unique=True, nullable=False)
@@ -25,4 +19,37 @@ class Event(db.Model):
     owner = db.Column(db.String(60), nullable=False)
     description = db.Column(db.String(200), nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    rsvps = db.relationship('Rsvp', backref='event')
 
+    def __init__(self, name, category, location, owner, description, user_id):
+        self.name = name
+        self.category = category
+        self.location = location
+        self.owner = owner
+        self.description = description
+        self.user_id = user_id
+
+
+class Rsvp(db.Model):
+    """
+        Create an Events table
+    """
+    __tablename__ = 'rsvps'
+    id = db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+
+    # Method to add attendees into the attendants list
+    def add_attendants(self, email):
+        attendant = Rsvp.query.filter_by(email=email).first()
+        if attendant:
+            # User has already made a reservation
+            return False
+        else:
+            db.session.add(attendant)
+            db.commit()
+
+    # Method to know the number of attendants
+    def get_total_attendants(self):
+        return db.session.query(func.count(Rsvp.id))
+        
