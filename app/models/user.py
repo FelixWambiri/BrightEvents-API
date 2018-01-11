1d9939de7dee84132ba3aa9a720ce5f505c4d8f4
Felix Wambiri <felixwambiri21@gmail.com>
2018-01-11 14:08:34 +0300

    [FIX] Fix query methods to extract the id from the user who is logged in

--- app/models/user.py
+++ app/models/user.py
@@ -49,31 +49,28 @@ class User(UserMixin, db.Model):
         """
         return check_password_hash(self.pw_hash, password)
 
-    def create_event(self, name, category, location, owner, description):
+    def create_event(self, name, category, location, description):
         """
         Method creates an event by first checking its existence and raising an integrity error if in existence
         :param name:
         :param category:
         :param location:
-        :param owner:
         :param description:
         :return:
         """
-        event_f = Event.query.filter_by(name=name).filter_by(owner=owner).first()
-        if event_f:
-            try:
-                event = Event(name=name, category=category, location=location, owner=owner, description=description)
-                db.session.add(event)
-                db.session.commit()
-                return True
-            except IntegrityError:
-                db.session.rollback()
-                print('There exists an event with that name already.Please choose another name')
 
-    def update_event(self, name, new_name, category, location, description, owner):
+        try:
+            event = Event(name=name, category=category, location=location, owner=self.id, description=description)
+            db.session.add(event)
+            db.session.commit()
+            return True
+        except IntegrityError:
+            db.session.rollback()
+            print('There exists an event with that name already.Please choose another name')
+
+    def update_event(self, name, new_name, category, location, description):
         """
         Method updates an event and if the field is empty it populates that field with previously stored info
-        :param owner:
         :param name:
         :param new_name:
         :param category:
@@ -82,7 +79,7 @@ class User(UserMixin, db.Model):
         :return:
         """
         try:
-            event = Event.query.filter_by(name=name).filter_by(owner=owner).first()
+            event = Event.query.filter_by(name=name).filter_by(owner=self.id).first()
             if new_name.strip():
                 event.name = new_name
 
@@ -99,40 +96,38 @@ class User(UserMixin, db.Model):
         except AttributeError:
             print('The event you want to update does not exist')
 
-    def delete_event(self, name, owner):
+    def delete_event(self, name):
         """
         Methods deletes an event give the name of the event
-        :param owner:
         :param name:
         :return:
         """
         try:
-            event = Event.query.filter_by(name=name).filter_by(owner=owner).one()
+            event = Event.query.filter_by(name=name).filter_by(owner=self.id).one()
             db.session.delete(event)
             db.session.commit()
         except NoResultFound:
             print("The event you are trying to delete does not exist")
 
-    def get_specific_event(self, name, owner):
+    def get_specific_event(self, name):
         """
         This method returns a specific event when given the events name
-        :param owner:
         :param name:
         :return:
         """
         try:
-            event = Event.query.filter_by(name=name).filter_by(owner=owner).first()
+            event = Event.query.filter_by(name=name).filter_by(owner=self.id).first()
             return "<Event(name='%s',category='%s',owner='%s')>" % (event.name, event.category, event.owner)
         except AttributeError:
             print('The event you are trying to retrieve does not exist')
             return False
 
-    def get_number_of_events(self, owner):
+    def get_number_of_events(self):
         """
         This method queries and returns the total number of events a person has created
         :return:
         """
-        return Event.query.filter_by(owner=owner).count()
+        return Event.query.filter_by(owner=self.id).count()
 
     def user_reset_password(self, new_pass):
         """
