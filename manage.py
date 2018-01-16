a90ab22b5246133406a180c95089681b6e2943eb
Felix Wambiri <felixwambiri21@gmail.com>
2018-01-16 21:45:13 +0300

    [ADD] Add script to handle database migration

--- manage.py
+++ manage.py
@@ -1,13 +1,20 @@
 """
 To write command-line tasks belonging outside the web app itself
 """
+from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
 # Getting the flask instance
 from app import app
+from app.init_db import db
+
+app.config.from_object('app.instance.config.DevelopmentConfig')
+
+migrate = Migrate(app, db)
 
 # Manager instance
 manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
