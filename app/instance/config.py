efe449ff5818acc8104bcd849e172a27f01166ff
Felix Wambiri <felixwambiri21@gmail.com>
2018-01-18 11:33:52 +0300

    [ADD] Refactor code to make it possible to export settings to the os

--- app/instance/config.py
+++ app/instance/config.py
@@ -1,30 +1,39 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 
 # Default config class
 class BaseConfig(object):
     DEBUG = False
+    CSRF_ENABLED = False
     SECRET_KEY = 'S\x883\xc6\x01\x07\xc5 \r\xd8\xab\\\xc4{$\xdf\xc6-\x8a\xd1\x85\xd1j'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:AndelaFellow2017@localhost/BrightEventDb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
 
 
-# Test config class that inherits from the default config class
 class TestingConfig(BaseConfig):
+    """ Testing configurations"""
     DEBUG = True
     TESTING = True
-    WTF_CSRF_ENABLED = False
     PRESERVE_CONTEXT_ON_EXCEPTION = False
-    SQLALCHEMY_ECHO = True  # Helps to debug by logging errors
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI')
 
 
-# Development config class that inherits from the default config class
 class DevelopmentConfig(BaseConfig):
+    """ Development configurations"""
     DEBUG = True
-    SQLALCHEMY_ECHO = True  # Helps to debug by logging errors
 
 
-# Production config class that inherits from the default config class
 class ProductionConfig(BaseConfig):
+    """ Production configurations"""
     DEBUG = False
+    TESTING = False
+    SQLALCHEMY_ECHO = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+}
