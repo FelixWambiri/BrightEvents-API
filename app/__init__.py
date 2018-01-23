f3bc25f89b04e79d292bc29a0126321dbdd4e136
Felix Wambiri <felixwambiri21@gmail.com>
2018-01-23 17:22:46 +0300

    [REF] Edit the name used to register the blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
-from app.auth import auth
-from app.events import event
+from app.auth import auth as auth_blueprint
+from app.events import event as event_blueprint
 from app.init_db import db
 
 from app.instance.config import app_config
@@ -11,6 +11,6 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     db.init_app(app)
-    app.register_blueprint(auth, url_prefix='/api/auth')
-    app.register_blueprint(event, url_prefix='/api')
+    app.register_blueprint(auth_blueprint, url_prefix='/api/auth')
+    app.register_blueprint(event_blueprint, url_prefix='/api')
     return app
