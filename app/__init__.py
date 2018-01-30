f5bccdbc7dfaf9cb02ab77bb37990f639eb73d26
Felix Wambiri <felixwambiri21@gmail.com>
2018-01-30 12:37:45 +0300

    [ADD] Add code to implement whoosh search indexing

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
-
+import flask_whooshalchemy as wa
 from app.auth import auth as auth_blueprint
 from app.events import event as event_blueprint
 from app.init_db import db
 
 from app.instance.config import app_config
+from app.models.event import Event
 
 
 def create_app(config_name):
@@ -13,4 +14,5 @@ def create_app(config_name):
     db.init_app(app)
     app.register_blueprint(auth_blueprint, url_prefix='/api/auth')
     app.register_blueprint(event_blueprint, url_prefix='/api')
+    wa.whoosh_index(app, Event)
     return app
