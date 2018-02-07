082cc2ed1cb86c236eafd82c424da77bd1cea027
Felix Wambiri <felixwambiri21@gmail.com>
2018-02-07 13:07:23 +0300

    [DEL] Remove method binding the app instance to flask-whooshalchemy index

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import flask_whooshalchemy as wa
 from app.auth import auth as auth_blueprint
 from app.events import event as event_blueprint
 from app.init_db import db
@@ -14,5 +13,4 @@ def create_app(config_name):
     db.init_app(app)
     app.register_blueprint(auth_blueprint, url_prefix='/api/auth')
     app.register_blueprint(event_blueprint, url_prefix='/api')
-    wa.whoosh_index(app, Event)
     return app
