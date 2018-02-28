071329b2b4b153081f508f2a41d32dbc494c18ad
Felix Wambiri <felixwambiri21@gmail.com>
2018-02-28 12:12:15 +0300

    [REF] Edit the stage cofiguration running the app to development

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ import os
 
 from app import create_app
 
-development = os.getenv('APP_SETTINGS')
-app = create_app(config_name='development')
+config_name = os.getenv('APP_SETTINGS')
+app = create_app(config_name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
