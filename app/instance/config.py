f2a2c5162df8d4d051e016ea0d29cc47afc7a392
Felix Wambiri <felixwambiri21@gmail.com>
2018-02-28 15:11:14 +0300

    [REF] Edit the database link

--- app/instance/config.py
+++ app/instance/config.py
@@ -6,7 +6,7 @@ class BaseConfig(object):
     DEBUG = False
     CSRF_ENABLED = False
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     MAIL_SERVER = 'smtp.googlemail.com'
