3d82914f4298c373f723f9ae7e1181197394edb3
Felix Wambiri <FelixWambiri>
2018-06-17 23:08:10 +0300

    [Chore] Solve cross origin errors

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from app.init_db import db
 from flask_mail import Mail
+from flask_cors import CORS
 
 from app.instance.config import app_config
 from app.models.event import Event
@@ -11,6 +12,7 @@ mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
     app.config.from_object(app_config[config_name])
     db.init_app(app)
     mail.init_app(app)
--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,6 +1,4 @@
 from flask import Blueprint
-from flask_cors import CORS
 
 auth = Blueprint('auth', __name__)
 from app.auth import views
-CORS(auth)
